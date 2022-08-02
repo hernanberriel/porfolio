c5d137e1b0c520e1631454397593e7ead0596c0c
Hernán Berrriel <80604133+hernanberriel@users.noreply.github.com>
2022-08-01 23:08:52 -0300

    Add files via upload

---
+++ porfolio/js/script.js
@@ -0,0 +1,40 @@
+// ***** MENSAJE DE INICIO ****
+
+function mensajeInicio(){
+    alert("Para una correcta visualizción de todo el contenido del sitio, recomiendo ingresar desde una PC o bien activar la vista de escritorio del navegador móvil.")
+}
+
+// ******* PORTAFOLIO SLIDER *****
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// **** FUNCTION CALL'S
+mensajeInicio()
+
